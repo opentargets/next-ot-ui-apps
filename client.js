cad1c1e25ca288041c1cc7ad782b5cec70582ab4
carcruz <me@carcruzcast.com>
2023-02-24 12:49:24 +0000

    feat: make initial ssr

---
+++ client.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.platform.dev.opentargets.xyz/api/v4/graphql",
+  cache: new InMemoryCache({ EntityUnionType: ["Drug"] }),
+  headers: { "OT-Platform": true },
+});
+
+export default client;
--- pages/drug/[id].js
+++ pages/drug/[id].js
@@ -1,21 +1,33 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router'
+import Head from "next/head";
+import DrugPageQuery from "./DrugPage.gql";
+import client from "../../client";
 
-export default function DP() {
-  const router = useRouter();
-  const { id } = router.query;
+export default function DP({ data }) {
+  const {
+    drug: { name, id, description },
+  } = data;
 
   return (
     <div>
       <Head>
         <title>Open Targets Platform</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={description} />
       </Head>
-
-      {/* TODO: Drug page should go here */}
-      Drug page {id}
-
-
+      Drug page {name} {id}
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  // Fetch data from external API
+  const { id } = context.query;
+
+  const data = await client.query({
+    query: DrugPageQuery,
+    variables: { chemblId: id },
+  });
+
+  // Pass data to the page via props
+  return { props: { data: data.data } };
 }
