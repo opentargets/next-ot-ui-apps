7eb6e8a90252d83a8b0fa3ef8dae821166ff62e6
Luca Fumis <lucaf@ebi.ac.uk>
2023-02-28 23:34:15 +0000

    WIP, missing files:  compiles; runtime errors

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://api.platform.dev.opentargets.xyz/api/v4/graphql",
+  // uri: "https://api.platform.dev.opentargets.xyz/api/v4/graphql",
+  uri: "https://api.platform.opentargets.org/api/v4/graphql",
   cache: new InMemoryCache({ EntityUnionType: ["Drug"] }),
   headers: { "OT-Platform": true },
 });
